3a5ebff9932d30a360b151e631f174f01cd2d9c5
Chr1stoph96 <christoph.meyer96@gmail.com>
2025-01-14 21:49:09 +0100

    Inaktive Benutzer -> kein Login mehr möglich

--- includes/login_logic.php
+++ includes/login_logic.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT password, role FROM users WHERE username = ?";
+    $sql = "SELECT password, role, status FROM users WHERE username = ?";
 
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('s', $username);
@@ -17,16 +17,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        $stmt->bind_result($stored_password_hash, $role);
+        $stmt->bind_result($stored_password_hash, $role, $status);
         $stmt->fetch();
 
         // Passwort überprüfen
         if (password_verify($password, $stored_password_hash)) {
-            $_SESSION['success'] = "Login erfolgreich!";
-            // Login erfolgreich
-            $_SESSION['user'] = $username; // Username in der Session speichern
-            $_SESSION['role'] = $role; // Rolle (admin oder user) in der Session speichern
-            header('Location: profile.php'); // Weiterleitung zum Profil
+            if ($status === 'aktiv') {
+                $_SESSION['success'] = "Login erfolgreich!";
+                // Login erfolgreich
+                $_SESSION['user'] = $username; // Username in der Session speichern
+                $_SESSION['role'] = $role; // Rolle (admin oder user) in der Session speichern
+                header('Location: profile.php'); // Weiterleitung zum Profil
+            } else {
+                $_SESSION['error'] = "Der Benutzer ist inaktiv.";    
+            }
         } else {
             $_SESSION['error'] = "Benutzername oder Passwort fehlerhaft.";    
         }
